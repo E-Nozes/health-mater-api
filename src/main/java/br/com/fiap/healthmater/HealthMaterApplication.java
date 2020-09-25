b01bdac153d9c4bd58df70b0c71dde7581aedd46
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2020-09-24 22:32:41 -0300

    Config properties

---
+++ src/main/java/br/com/fiap/healthmater/HealthMaterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.healthmater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/healthmater/HealthMaterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.healthmater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
